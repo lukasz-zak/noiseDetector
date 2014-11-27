396b44126f6c4d35978c2dc52f2c375ed21b1a96
Łukasz Żak <lukasz.zak@schibsted.pl>
2014-11-27 12:20:33 +0100

    Add login form

--- app/scripts/client.js
+++ app/scripts/client.js
@@ -3,78 +3,96 @@ var analyser;
 var microphone;
 var siarapGraphValue = document.getElementById('siarap-graph-value');
 var siarapMaxVolumeValue = document.getElementById('siarap-max-volume');
-var maxNoise = 10;
+
+var input = $('#userName');
+var detector = $('#detector');
+detector.hide();
 
 var pn = PUBNUB.init({
   publish_key   : 'pub-c-e3b482ce-85af-4325-af51-102d54b809ae',
   subscribe_key : 'sub-c-0bad17ea-761b-11e4-a5ea-02ee2ddab7fe'
 });
 
-pn.publish({
-  channel : 'users',
-  message : {'user' : navigator.userAgent}
+
+$('#submit').click(function (e){
+  e.preventDefault();
+
+  console.log('input.val()', input.val());
+
+  pn.publish({
+    channel : 'users',
+    message : {
+      'user' : navigator.userAgent,
+      'area' : input.val()
+    }
+  });
 });
 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+var maxNoise = 10;
 
-if (navigator.getUserMedia) {
+var init = function (){
 
-  navigator.getUserMedia(
-    {audio: true},
-    function(stream) {
-      aCtx = new (window.AudioContext || window.webkitAudioContext)();
-      analyser = aCtx.createAnalyser();
-      analyser.smoothingTimeConstant  = 0.3;
-      analyser.fftSize = 1024;
-      microphone = aCtx.createMediaStreamSource(stream);
-      microphone.connect(analyser);
+  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+
+  if (navigator.getUserMedia) {
+
+    navigator.getUserMedia(
+      {audio: true},
+      function(stream) {
+        aCtx = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = aCtx.createAnalyser();
+        analyser.smoothingTimeConstant  = 0.3;
+        analyser.fftSize = 1024;
+        microphone = aCtx.createMediaStreamSource(stream);
+        microphone.connect(analyser);
 //      analyser.connect(aCtx.destination);
-      process();
-    }, function (){}
-  );
-}
-
-function getAverageVolume(array) {
-  var values = 0;
-  var average;
-
-  var length = array.length;
-
-  // get all the frequency amplitudes
-  for (var i = 0; i < length; i++) {
-    values += array[i];
+        process();
+      }, function (){}
+    );
   }
 
-  average = values / length;
-  return average;
-}
+  function getAverageVolume(array) {
+    var values = 0;
+    var average;
 
-function process(){
-  setInterval(function(){
-    var array =  new Uint8Array(analyser.frequencyBinCount);
-    analyser.getByteFrequencyData(array);
+    var length = array.length;
 
-    var average = getAverageVolume(array);
-
-    var currNoise = average / maxNoise * 100;
-
-    if(average > maxNoise) {
-      maxNoise = average;
+    // get all the frequency amplitudes
+    for (var i = 0; i < length; i++) {
+      values += array[i];
     }
 
-    siarapGraphValue.style.width = currNoise + '%';
-    siarapMaxVolumeValue.textContent = Math.round(maxNoise, 2);
+    average = values / length;
+    return average;
+  }
 
-    if(currNoise >= 40) {
-      siarapGraphValue.style.backgroundColor = 'red';
-    } else {
-      siarapGraphValue.style.backgroundColor = 'green';
-    }
+  function process(){
+    setInterval(function(){
+      var array =  new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(array);
 
-    pn.publish({
-      channel: 'demo_tutorial',
-      message: {"volume": currNoise}
-    });
+      var average = getAverageVolume(array);
 
-  },1000);
-}
+      var currNoise = average / maxNoise * 100;
+
+      if(average > maxNoise) {
+        maxNoise = average;
+      }
+
+      siarapGraphValue.style.width = currNoise + '%';
+      siarapMaxVolumeValue.textContent = Math.round(maxNoise, 2);
+
+      if(currNoise >= 40) {
+        siarapGraphValue.style.backgroundColor = 'red';
+      } else {
+        siarapGraphValue.style.backgroundColor = 'green';
+      }
+
+      pn.publish({
+        channel: 'demo_tutorial',
+        message: {"volume": currNoise}
+      });
+
+    },1000);
+  }
+};
--- app/scripts/server.js
+++ app/scripts/server.js
@@ -3,10 +3,20 @@ var pn = PUBNUB.init({
   subscribe_key : 'sub-c-0bad17ea-761b-11e4-a5ea-02ee2ddab7fe'
 });
 
+var generateHtmlForNewUser = function (areaName, ua){
+  return $('<a href="#" class="list-group-item">' +
+      '<h4 class="list-group-item-heading">'+ areaName +'</h4>' +
+      '<p class="list-group-item-text">'+ ua +'</p>' +
+    '</a>'
+  );
+};
+
 pn.subscribe({
   channel: 'users',
   message: function(m){
     console.info('User join', m.user);
+
+    $('.list-group').append(generateHtmlForNewUser(m.area, m.user));
   }
 });
 
